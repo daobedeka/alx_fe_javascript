43f40ba4d5e63252541a57cb78a740d24b6e912c
Daniel Obedeka <daobedeka@gmail.com>
2025-02-01 21:21:15 +0000

    Started task1

---
+++ dom-manipulation/script.js
@@ -0,0 +1,7 @@
+let newQoute=document.getElementById('newQuote');
+let newQuoteText=document.getElementById('newQuoteText').value.trim();
+let  newQuoteCategory=document.getElementById('newQuoteCategory').value.trim();
+
+function showRandomQuote(){
+    
+}
