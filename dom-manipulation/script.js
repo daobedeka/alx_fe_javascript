47636e05caba33bfc5ef5376c0b34acef063ec73
Daniel Obedeka <daobedeka@gmail.com>
2025-02-02 23:24:50 +0000

    made some changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,7 +1,34 @@
 let newQoute=document.getElementById('newQuote');
-let newQuoteText=document.getElementById('newQuoteText').value.trim();
-let  newQuoteCategory=document.getElementById('newQuoteCategory').value.trim();
+let newQuoteText=document.getElementById('newQuoteText');
+let  newQuoteCategory=document.getElementById('newQuoteCategory');
+let Stoeredquote=JSON.parse(localStorage.getItem('quote'))||[];
+const quoteDisplay=document.getElementById('quoteDisplay')
+
 
 function showRandomQuote(){
+quote.forEach(element => {
+   let h2=document.createElement('h2');
+   let p=document.createElement('p');
+    h2.textContent=element.category;
+    p.textContent=element.text;
+
+});
     
+}
+
+function addQuote(){
+
+  if(newQuoteText.value!=null && newQuoteCategory.value)
+     {
+              let quote={ };
+              quote.text=newQuoteText.value;
+              quote.category=newQuoteCategory.value;
+                
+              console.log(quote)
+            }
+            else{
+              alert('Enter information')
+            }
+
+
 }
