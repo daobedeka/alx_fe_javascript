320d8c10fb97c978f8e79785a5dee6a82e20054d
Daniel Obedeka <daobedeka@gmail.com>
2025-02-05 17:23:47 +0000

    completed task2

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -60,6 +60,17 @@ function exportToJsonFile(){
 
 }
 
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert('Quotes imported successfully!');
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
+
 
 function createAddQuoteForm(){
   
